a180ad5e3b65311edeecde68ff330214b13d32d8
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-01-17 18:40:45 +0330

    update|order|models

--- shopping/order/models.py
+++ shopping/order/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from  customer.models import MyUser
+from product.models import DiscountCode,Product
 
-# Create your models here.
+
+
+class Order(models.Model):
+
+    STATUS_CHOICES = [
+        ('کارت', 'کارت'),
+        ('پرداخت', 'پرداخت'),
+        ('تسویه', 'تسویه'),
+    ]
+
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE,related_name="orders")
+    discount_code=models.OneToOneField(DiscountCode,on_delete=models.CASCADE,related_name="orders")
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='CART')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+   
+    product=models.ForeignKey(Product,on_delete=models.CASCADE,related_name="order_items")
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name="order_items")
+    quantity=models.IntegerField(default=1)
