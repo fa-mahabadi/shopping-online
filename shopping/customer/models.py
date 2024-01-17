4ba09b447906d7d96ba977357bf2cffc6b693da0
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-01-17 18:38:31 +0330

    update|customer|model

--- shopping/customer/models.py
+++ shopping/customer/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import Group
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+        if user.is_supervisor:
+            supervisor_group, created = Group.objects.get_or_create(name='SUPERVISOR')
+            user.groups.add(supervisor_group)
+
+        if user.is_product_manager:
+            product_manager_group, created = Group.objects.get_or_create(name='PRODUCT_MANAGER')
+            user.groups.add(product_manager_group)
+
+        if user.is_operator:
+            operator_group, created = Group.objects.get_or_create(name='OPERATOR')
+            user.groups.add(operator_group)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class MyUser(AbstractUser):
+    
+    email = models.EmailField(unique=True)
+    is_supervisor = models.BooleanField(default=False)
+    is_product_manager = models.BooleanField(default=False)
+    # operator is superuser
+    is_operator = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class Address(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name="addresses")
+    city = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    detail = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=12)
