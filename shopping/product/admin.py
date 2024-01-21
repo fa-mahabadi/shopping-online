5133d2a8564273eb73f2e11893bb5746b22b8063
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-01-21 15:51:09 +0330

    update|product|customize admin panel

--- shopping/product/admin.py
+++ shopping/product/admin.py
@@ -1,31 +1,66 @@
-from django.contrib import admin
-from .models import Category , Product ,Discount, DiscountCode, Comment, Image
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Discount)
-admin.site.register(DiscountCode)
-admin.site.register(Comment)
-
+from .models import Category, Product, Discount, DiscountCode, Comment, Image
 from django.contrib import admin
 from .models import Image
 
+
+# Register your models here.
+@admin.register(Category)
+class MyCategory(admin.ModelAdmin):
+    list_display = ["name", "parent_category", "discount"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Product)
+class MyProduct(admin.ModelAdmin):
+    list_display = [
+        "category",
+        "discount",
+        "total_count",
+        "name",
+        "brand",
+        "price",
+        "color",
+        "size",
+    ]
+    list_filter = ["category"]
+    search_fields = ["name"]
+
+
+@admin.register(Discount)
+class MyDiscount(admin.ModelAdmin):
+    list_display = ["name", "percentage", "code", "cash_amount", "expire", "maximum"]
+    list_filter = ["percentage"]
+    search_fields = ["name"]
+
+
+@admin.register(DiscountCode)
+class MyDiscountCode(admin.ModelAdmin):
+    list_display = ["name", "percentage", "code", "cash_amount", "expire", "maximum"]
+    list_filter = ["percentage"]
+    search_fields = ["name"]
+
+
+@admin.register(Comment)
+class MyComment(admin.ModelAdmin):
+    list_display = ["product", "reply_to", "content", "email"]
+    list_filter = ["product"]
+    search_fields = ["product"]
+
+
+@admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    readonly_fields = ['thumbnail_preview']
-    list_display = ['thumbnail_preview']
+    readonly_fields = ["thumbnail_preview"]
+    list_display = ["thumbnail_preview"]
 
     def thumbnail_preview(self, obj):
         return obj.thumbnail_preview
 
-    thumbnail_preview.short_description = 'Preview'
+    thumbnail_preview.short_description = "پیش نمایش"
     thumbnail_preview.allow_tags = True
 
     def image_preview(self, obj):
         return obj.image_preview()
 
-    image_preview.short_description = 'Image Preview'
+    image_preview.short_description = "Image Preview"
     image_preview.allow_tags = True
-
-admin.site.register(Image, ImageAdmin)
-
-
--- shopping/product/models.py
+++ shopping/product/models.py
@@ -3,39 +3,63 @@ from django.utils.html import mark_safe
 
 
 class Discount(models.Model):
-    name=models.CharField(null=True,blank=True, max_length=100)
-    percentage = models.PositiveIntegerField(null=True,blank=True)
-    code = models.CharField(null=True, max_length=100,blank=True)
-    cash_amount = models.FloatField(null=True,blank=True)
-    expire = models.DateTimeField(null=True,blank=True)
-    maximum = models.FloatField(default=0)
+    name = models.CharField(null=True, blank=True, max_length=100, verbose_name="نام")
+    percentage = models.PositiveIntegerField(
+        null=True, blank=True, verbose_name="درصدی"
+    )
+    code = models.CharField(
+        null=True, max_length=100, blank=True, verbose_name="کد تخفیف"
+    )
+    cash_amount = models.FloatField(null=True, blank=True, verbose_name="نقدی")
+    expire = models.DateTimeField(null=True, blank=True, verbose_name="تاریخ انقضاء")
+    maximum = models.FloatField(default=0, verbose_name="مقدار بیشینه")
 
     def __str__(self):
         return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "  تخفیف ها"
+        verbose_name = " تخفیف"
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="نام")
     parent_category = models.ForeignKey(
-        "self", null=True, blank=True, on_delete=models.SET_NULL
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        verbose_name="دسته بندی",
     )
-    discount = models.OneToOneField(Discount, on_delete=models.CASCADE)
+    discount = models.OneToOneField(
+        Discount, on_delete=models.CASCADE, verbose_name="تخفیف"
+    )
 
     def __str__(self):
         return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "دسته بندی ها"
+        verbose_name = "دسته بندی"
 
 
 class Product(models.Model):
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="products"
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="دسته بندی",
     )
     discount = models.ForeignKey(
-        Discount, on_delete=models.CASCADE, related_name="products"
+        Discount,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="تخفیف",
     )
-    total_count = models.PositiveIntegerField()
-    name = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100)
-    price = models.FloatField()
+    total_count = models.PositiveIntegerField(verbose_name="تعداد کل")
+    name = models.CharField(max_length=100, verbose_name="نام")
+    brand = models.CharField(max_length=100, verbose_name="برند")
+    price = models.FloatField(verbose_name="قیمت")
     colors = [
         ("سفید", "سفید"),
         ("مشکی", "مشکی"),
@@ -43,20 +67,24 @@ class Product(models.Model):
         ("قرمز", "قرمز"),
         ("سبز", "سبز"),
     ]
-    color = models.CharField(choices=colors, max_length=10)
+    color = models.CharField(choices=colors, max_length=10, verbose_name="رنگ")
     sizes = [("M", "M"), ("L", "L"), ("XL", "XL"), ("XXL", "XXL")]
-    size = models.CharField(choices=sizes, max_length=10)
+    size = models.CharField(choices=sizes, max_length=10, verbose_name="سایز")
 
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        verbose_name_plural = "محصولات"
+        verbose_name = "محصول"
+
 
 class Image(models.Model):
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name="images"
+        Product, on_delete=models.CASCADE, related_name="images", verbose_name="محصول"
     )
     image = models.ImageField(
-        verbose_name="ItemImage", upload_to="images/product", null=True, blank=True
+        verbose_name="عکس", upload_to="images/product", null=True, blank=True
     )
 
     # def image_preview(self):
@@ -68,28 +96,46 @@ class Image(models.Model):
                 '<img src= "{}" width="200" height="200" />'.format(self.image.url)
             )
 
+    class Meta:
+        verbose_name_plural = "تصاویر"
+        verbose_name = "تصویر"
+
 
 class DiscountCode(models.Model):
-    name=models.CharField(null=True,blank=True, max_length=100)
-    percentage = models.PositiveIntegerField(null=True,blank=True)
-    code = models.CharField(null=True, max_length=100,blank=True)
-    cash_amount = models.FloatField(null=True,blank=True)
-    expire = models.DateTimeField(null=True,blank=True)
-    maximum = models.FloatField(default=0)
+    name = models.CharField(null=True, blank=True, max_length=100, verbose_name="نام")
+    percentage = models.PositiveIntegerField(
+        null=True, blank=True, verbose_name="درصدی"
+    )
+    code = models.CharField(null=True, max_length=100, blank=True, verbose_name="کد")
+    cash_amount = models.FloatField(null=True, blank=True, verbose_name="نقدی")
+    expire = models.DateTimeField(null=True, blank=True, verbose_name="انقضاء")
+    maximum = models.FloatField(default=0, verbose_name="مقدار بیشینه")
 
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        verbose_name_plural = "کدهای تخفیف"
+        verbose_name = "کد تخفیف"
+
 
 class Comment(models.Model):
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name="comments"
+        Product, on_delete=models.CASCADE, related_name="comments", verbose_name="محصول"
     )
     reply_to = models.ForeignKey(
-        "self", null=True, on_delete=models.SET_NULL, related_name="comments"
+        "self",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="comments",
+        verbose_name="پاسخ به",
     )
-    content = models.CharField(max_length=200)
-    email = models.EmailField()
+    content = models.CharField(max_length=200, verbose_name="متن")
+    email = models.EmailField(verbose_name="ایمیل")
 
     def __str__(self):
         return f"{self.product.name}"
+
+    class Meta:
+        verbose_name_plural = "نظرات"
+        verbose_name = "نظر"
