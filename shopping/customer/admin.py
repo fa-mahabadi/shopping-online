3aeb435eade15edbf24b68d8413c7acb041252a2
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-01-21 15:48:30 +0330

    update|customer|customize admin panel

--- shopping/customer/admin.py
+++ shopping/customer/admin.py
@@ -1,37 +1,55 @@
-from django.contrib.auth.models import Group
-from django.contrib import admin
-from .models import MyUser,Address
-# Register your models here.
-# admin.site.register(MyUser)
-admin.site.register(Address)
+from .models import MyUser, Address
+
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import MyUser
 
 
+@admin.register(MyUser)
 class MyUserAdmin(UserAdmin):
-    model = MyUser
-    list_display = ['email', 'is_supervisor', 'is_product_manager', 'is_operator']
-
+    list_display = ["email", "is_supervisor", "is_product_manager", "is_operator"]
+    search_fields = ("email",)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                    'groups', 'user_permissions')}),
-        ('Custom Fields', {'fields': ('is_supervisor', 'is_product_manager', 'is_operator')}),
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Custom Fields",
+            {"fields": ("is_supervisor", "is_product_manager", "is_operator")},
+        ),
     )
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_supervisor', 'is_product_manager', 'is_operator'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_supervisor",
+                    "is_product_manager",
+                    "is_operator",
+                ),
+            },
+        ),
     )
 
-admin.site.register(MyUser, MyUserAdmin)
 
-
-
-
-
-
-
+@admin.register(Address)
+class MyAddress(admin.ModelAdmin):
+    list_display = ["user", "city", "province", "detail", "zipcode"]
+    list_filter = ["city"]
+    search_fields = ["city", "user"]
