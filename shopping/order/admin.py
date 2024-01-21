d1227c2758f056466ebaaac583ca5985ea7e9367
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-01-21 15:49:41 +0330

    update|order|customize admin panel

--- shopping/order/admin.py
+++ shopping/order/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Order,OrderItem
+from .models import Order, OrderItem
+
+
 # Register your models here.
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(Order)
+class MyOrder(admin.ModelAdmin):
+    list_display = ["user", "discount_code", "status"]
+    readonly_fields = ["created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["status"]
+
+
+@admin.register(OrderItem)
+class MyOrderItem(admin.ModelAdmin):
+    list_display = ["product", "order", "quantity"]
+    list_filter = ["product"]
+    search_fields = ["product"]
