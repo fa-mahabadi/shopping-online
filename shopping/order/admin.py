1762a976603e03484c875cb531c8e99566197e8e
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-01-21 19:19:23 +0330

    update|order|admin panel

--- shopping/order/admin.py
+++ shopping/order/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Order,OrderItem
+from .models import Order, OrderItem
+
+
 # Register your models here.
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user", "discount_code", "status", "created_at"]
+    search_fields = ("user",)
+    list_filter = ("status",)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["product", "order", "quantity"]
+    search_fields = ("product",)
+    list_filter = ("product",)
--- shopping/order/models.py
+++ shopping/order/models.py
@@ -1,25 +1,49 @@
 from django.db import models
-from  customer.models import MyUser
-from product.models import DiscountCode,Product
-
+from customer.models import MyUser
+from product.models import DiscountCode, Product
 
 
 class Order(models.Model):
-
     STATUS_CHOICES = [
-        ('کارت', 'کارت'),
-        ('پرداخت', 'پرداخت'),
-        ('تسویه', 'تسویه'),
+        ("کارت", "کارت"),
+        ("پرداخت", "پرداخت"),
+        ("تسویه", "تسویه"),
     ]
 
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE,related_name="orders")
-    discount_code=models.OneToOneField(DiscountCode,on_delete=models.CASCADE,related_name="orders")
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='CART')
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        MyUser, on_delete=models.CASCADE, related_name="orders", verbose_name="کاربر"
+    )
+    discount_code = models.OneToOneField(
+        DiscountCode,
+        on_delete=models.CASCADE,
+        related_name="orders",
+        verbose_name="کدتخفیف",
+    )
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="CART", verbose_name="وضعیت"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
+
+    class Meta:
+        verbose_name_plural = " سفارشات  "
+        verbose_name = " سفارش"
 
 
 class OrderItem(models.Model):
-   
-    product=models.ForeignKey(Product,on_delete=models.CASCADE,related_name="order_items")
-    order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name="order_items")
-    quantity=models.IntegerField(default=1)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="order_items",
+        verbose_name="محصول",
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="order_items",
+        verbose_name="سفارش",
+    )
+    quantity = models.IntegerField(default=1, verbose_name="تعداد سفارش")
+
+    class Meta:
+        verbose_name_plural = "آیتم های سفارش  "
+        verbose_name = "آیتم سفارش"
