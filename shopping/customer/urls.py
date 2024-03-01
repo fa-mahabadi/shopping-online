16ecd209ec63c6678d2e57a2435d79849f9b3b7c
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-03-01 09:44:37 +0330

    update | customer|url

--- shopping/customer/urls.py
+++ shopping/customer/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("login/", views.MyLoginView.as_view(), name="login"),
+    path("logout/", views.MyLogoutView.as_view(), name="logout"),
+    path("shopping_cart/", views.shopping_cart, name="shopping_cart"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path('verify_code/', views.Verify_code.as_view(), name='verify_code'),
+    path('profile/create/', views.create_profile, name='create_profile'),
+    path('address/api/',views.AddressAPIView.as_view(),name="address_list"),
+    path('address/api/<int:pk>/', views.AddressDetailAPIview.as_view(), name='address_detail'),
+  
+   
 ]
