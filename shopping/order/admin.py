9009b1927605e0ec2e766380c814238a5b487bd2
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-03-01 09:47:06 +0330

    update | order|admin

--- shopping/order/admin.py
+++ shopping/order/admin.py
@@ -5,13 +5,15 @@ from .models import Order, OrderItem
 # Register your models here.
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["user", "discount_code", "status", "created_at"]
+    list_display = ["user", "discount_code", "status", "created_at","is_active",
+        "is_deleted",]
     search_fields = ("user",)
     list_filter = ("status",)
 
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ["product", "order", "quantity"]
+    list_display = ["product", "order", "quantity","is_active",
+        "is_deleted",]
     search_fields = ("product",)
     list_filter = ("product",)
