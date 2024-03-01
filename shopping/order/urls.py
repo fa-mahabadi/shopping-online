b61ac4ef0232878939c62bd81c992d0ab1200249
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-03-01 09:50:27 +0330

    update | order|url

--- shopping/order/urls.py
+++ shopping/order/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    
+    path("create/",views.OrderAPIView.as_view(),name="order_create"),
+    path("history/",views.history_order,name="order_history"),
+    path('items/', views.OrderItemAPIView.as_view(), name='order_items'),
+    path('items/<int:pk>/', views.OrderItemDetailAPIView.as_view(), name='order_item_detail'),
 ]
+
+    
