65ffd228a1dfbe5f9ae2253b4be993484ebf0647
fa-mahabadi <fa.mahabadi@yahoo.com>
2024-03-01 09:56:04 +0330

    update | shopping | url

--- shopping/shopping/urls.py
+++ shopping/shopping/urls.py
@@ -18,11 +18,19 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/', include('customer.urls')),
     path("order/",include("order.urls")),
-    path("product/",include("product.urls")),
-    path("core/",include("core.urls"))
+    path("",include("product.urls")),
+    path("core/",include("core.urls")),
+    path('api-auth/', include('rest_framework.urls')),
+    # YOUR PATTERNS
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
